f5a51ac28dd6948e2f6a0038dbbba123f83ee89e
Cai Jie <jiecai@sty.pku.edu.cn>
2024-04-21 23:13:40 +0800

    lab2

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -6,6 +6,7 @@ package DebugExercise;
  */
 public class DebugExercise1 {
     public static int divideThenRound(int top, int bottom) {
+        if (top == 0 || bottom == 0) { return 0; }
         return Math.round((float)top/bottom);
     }
 
@@ -24,5 +25,10 @@ public class DebugExercise1 {
         int b3 = 4;
         int result3 = divideThenRound(t3, b3);
         System.out.println("round(" + t3 + "/" + b3 + ")=" + result3);
+
+        int t4 = 1;
+        int b4 = 0;
+        int result4 = divideThenRound(t4, b4);
+        System.out.println("round(" + t4 + "/" + b4 + ")=" + result4);
     }
 }
